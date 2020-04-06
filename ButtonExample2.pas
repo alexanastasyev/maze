1631ea4377e5c21b458576267db7fec3e0020518
alexanastasyev <58271227+alexanastasyev@users.noreply.github.com>
2020-04-06 14:24:52 +0300

    Working button examples
    - "ButtonExample1.pas" is an object-oriented and nice button; not recommended for course work
    - "ButtonExample2.pas" is a procedure but more ugly button; suits better

---
+++ ButtonExample2.pas
@@ -0,0 +1,21 @@
+﻿uses graphABC;
+var x1,y1,x2,y2:integer;
+procedure MouseDown(x,y,mousebutton: integer);
+begin
+if(x in [x1..x2])and(y in [y1..y2]) then
+ begin
+  setpencolor(clBlue);
+  setbrushcolor(clBlue);
+  circle(200,200,100);
+ end;
+end; 
+begin
+x1:=10;
+y1:=440;
+x2:=120;
+y2:=470;
+setpencolor(clGreen);
+setbrushcolor(clGreen); 
+rectangle(x1,y1,x2,y2);
+onmousedown:=MouseDown;
+end.
