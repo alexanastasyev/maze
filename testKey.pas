25386b64abcf494ff8494d57418a14b58d2b8141
alexanastasyev <58271227+alexanastasyev@users.noreply.github.com>
2020-04-07 18:49:33 +0300

    "testKey.pas" added
    "testKey.pas" is an example of OnKeyDown working inside another OnKeyDown

---
+++ testKey.pas
@@ -0,0 +1,33 @@
+﻿{
+  Here OnKeyDown works inside OnMouseDown
+}
+
+Program TestKey;
+
+Uses
+  GraphABC;
+
+Procedure MakeMove(key: integer);
+begin
+
+  writeln('KeyDown body');  
+  setpencolor(clBlue);
+  setbrushcolor(clBlue);
+  circle(200,200,100);
+  
+end;
+
+Procedure PlayGame(x: integer);
+begin
+  
+  writeln('MouseDown body');
+  OnKeyDown:= MakeMove;
+  
+end;
+
+Begin
+  
+  writeln('It`s main body');
+  OnKeyDown:= PlayGame;
+  
+end.
